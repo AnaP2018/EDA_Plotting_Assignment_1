735f0d9933201105c7764d09995a0434bc740d27
AnaP2018 <ana@starrweb.uk>
2018-09-09 08:57:07 +0100

    new files

---
+++ Plot2.R
@@ -0,0 +1,30 @@
+#Week 1 Project - EDA - Data Science - Plot 2
+
+#####Loading and preparing the data        
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+tempf <- tempfile()
+download.file(url,tempf)
+unzip(tempf)3w
+unlink(tempf)
+
+#read text file, separator ";", missing values "?"
+dataw1 <- read.csv("household_power_consumption.txt",header=T,sep=';',na.strings = "?")
+
+#subset data to only include the 2 relevant days
+ dataw1<-subset(dataw1,Date %in% c("1/2/2007","2/2/2007"))
+
+#Convert Date and Time into datetime
+dataw1$Date <- as.Date(dataw1$Date, format = "%d/%m/%Y")
+dataw1$datetime <- strptime(paste(dataw1$Date, dataw1$Time), "%Y-%m-%d %H:%M:%S")
+
+#Plot2
+with(dataw1,plot(datetime,Global_active_power, type = "l",
+	ylab = "Global Active Power (kilowatts)", xlab = ""))
+
+#Copy to png file
+dev.copy(png, file = "plot2.png", height = 480, width = 480)
+dev.off()
+
+
+
+
